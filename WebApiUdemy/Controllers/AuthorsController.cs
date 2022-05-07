c40e93133fb48eee1359f9da5b46ac67d5704941
estmontenegro1 <estmontenegro1@gmail.com>
2022-05-07 00:36:22 -0500

    Add inicial

--- WebApiUdemy/Controllers/AuthorsController.cs
+++ WebApiUdemy/Controllers/AuthorsController.cs
@@ -1,14 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApiUdemy.Entities;
 
 namespace WebApiUdemy.Controllers
 {
     [ApiController]
+    [Route("api/authors")]
     public class AuthorsController : ControllerBase
     {
+        private readonly ApplicationDbContext context;
 
+        public AuthorsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Author>>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Author autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
